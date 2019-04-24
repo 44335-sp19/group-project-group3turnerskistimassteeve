20f68aec5a7ad4342d35570d15df38bb10a0bebb
coltonsteeve <coltonsteeve@gmail.com>
2019-04-23 22:05:55 -0500

    Tried to fix JavaScript

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
-$(document).ready(function () {
+/*$(document).ready(function () {
     $('.carousel').flickity({
         // options
         cellAlign: 'left',
         contain: true
-      });  
-});
+      });
+
+    $('#coltonprog').click(function () { 
+
+      $('#coltonprogp').css("background-color", "blue");
+      
+    });
+});*/
+
+var x = document.getElementById("coltonname");
+x.innerHTML = "Hello";
+
+
+
+
+
