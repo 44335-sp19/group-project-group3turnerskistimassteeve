7bb907f0a8dfe971561a66efe72e27702f0f4520
coltonsteeve <coltonsteeve@gmail.com>
2019-04-25 16:46:21 -0500

    Added placeholders for other group members on home page

--- main.js
+++ main.js
@@ -14,6 +14,14 @@ $(document).ready(function () {
       
     });
 
+    $('#coltondesignp').hide();
+
+    $('#coltondesign').click(function () { 
+
+      $('#coltondesignp').toggle();
+      
+    });
+
 });
 
 document.getElementById('coltonindexpic').addEventListener('mouseover', function(){
@@ -22,6 +30,26 @@ document.getElementById('coltonindexpic').addEventListener('mouseover', function
 });
 
 document.getElementById('coltonindexpic').addEventListener('mouseout', function(){
+  this.style.transform = 'scale(1.0)';
+  this.style.transition = 'transform .2s';
+});
+
+document.getElementById('cassidyindexpic').addEventListener('mouseover', function(){
+  this.style.transform = 'scale(1.5)';
+  this.style.transition = 'transform .2s';
+});
+
+document.getElementById('cassidyindexpic').addEventListener('mouseout', function(){
+  this.style.transform = 'scale(1.0)';
+  this.style.transition = 'transform .2s';
+});
+
+document.getElementById('kenyaindexpic').addEventListener('mouseover', function(){
+  this.style.transform = 'scale(1.5)';
+  this.style.transition = 'transform .2s';
+});
+
+document.getElementById('kenyaindexpic').addEventListener('mouseout', function(){
   this.style.transform = 'scale(1.0)';
   this.style.transition = 'transform .2s';
 });
