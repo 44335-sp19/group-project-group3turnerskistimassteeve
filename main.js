d6a78f63616edb4c42d6f8c6e82e714c335b98c4
coltonsteeve <coltonsteeve@gmail.com>
2019-04-25 18:24:34 -0500

    Tried to make slideDown work.

--- main.js
+++ main.js
@@ -1,66 +1,61 @@
-$(document).ready(function () {
-    $('.carousel').flickity({
-        // options
-        cellAlign: 'left',
-        contain: true
-      });
+jQuery(document).ready(function () {
 
+  $('#coltonprogp').hide();
 
-    $('#coltonprogp').hide();
+  jQuery('#coltonprog').click(function () {
 
-    $('#coltonprog').click(function () {
+    jQuery('#coltonprogp').toggle();
 
-      $('#coltonprogp').toggle();
+  });
 
-    });
+  $('#coltondesignp').hide();
 
-    $('#coltondesignp').hide();
+  $('#coltondesign').click(function () {
 
-    $('#coltondesign').click(function () {
+    $('#coltondesignp').toggle();
 
-      $('#coltondesignp').toggle();
-
-    });
+  });
 
 });
-$(document).ready(function(){
-  $("#button2Hide").click(function(){
+
+$(document).ready(function () {
+  $("#button2Hide").click(function () {
     $(".p2").toggle();
   });
 });
 
-document.getElementById('coltonindexpic').addEventListener('mouseover', function(){
+document.getElementById('coltonindexpic').addEventListener('mouseover', function () {
   this.style.transform = 'scale(1.5)';
   this.style.transition = 'transform .2s';
 });
 
-document.getElementById('coltonindexpic').addEventListener('mouseout', function(){
+document.getElementById('coltonindexpic').addEventListener('mouseout', function () {
   this.style.transform = 'scale(1.0)';
   this.style.transition = 'transform .2s';
 });
 
-document.getElementById('cassidyindexpic').addEventListener('mouseover', function(){
+document.getElementById('cassidyindexpic').addEventListener('mouseover', function () {
   this.style.transform = 'scale(1.5)';
   this.style.transition = 'transform .2s';
 });
 
-document.getElementById('cassidyindexpic').addEventListener('mouseout', function(){
+document.getElementById('cassidyindexpic').addEventListener('mouseout', function () {
   this.style.transform = 'scale(1.0)';
   this.style.transition = 'transform .2s';
 });
 
-document.getElementById('kenyaindexpic').addEventListener('mouseover', function(){
+document.getElementById('kenyaindexpic').addEventListener('mouseover', function () {
   this.style.transform = 'scale(1.5)';
   this.style.transition = 'transform .2s';
 });
 
-document.getElementById('kenyaindexpic').addEventListener('mouseout', function(){
+document.getElementById('kenyaindexpic').addEventListener('mouseout', function () {
   this.style.transform = 'scale(1.0)';
   this.style.transition = 'transform .2s';
 });
 
-$(document).ready(function(){
-  $("#button2Hide").click(function(){
+$(document).ready(function () {
+  $("#button2Hide").click(function () {
     $(".p2").toggle();
   });
 });
